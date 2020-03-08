9c0bd29aeaa26b53343b3b4063c643693d8b25f3
hreso110100 <hreskodavid57@gmail.com>
2020-03-08 22:15:19 +0100

    model verification added

--- src/neural_network/lstm.py
+++ src/neural_network/lstm.py
@@ -1,4 +1,9 @@
+from datetime import datetime
+
 from keras import Sequential
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.preprocessing import MinMaxScaler
 from keras.layers import LSTM, Dense, Dropout
 
 
@@ -6,28 +11,77 @@ class LSTMBuilder:
 
     def __init__(self, back_step: int, future_step: int):
         self.model = None
+        self.features = 5
         self.back_step = back_step
         self.future_step = future_step
 
+    # building model
     def build(self):
         self.model = Sequential()
-        self.model.add(LSTM(30, input_shape=(self.back_step, 5), return_sequences=True))
-        self.model.add(LSTM(12, return_sequences=True))
-        self.model.add(LSTM(12, return_sequences=True))
-        self.model.add(Dropout(0.5))
-        self.model.add(LSTM(12, return_sequences=True))
-        self.model.add(LSTM(12, return_sequences=True))
-        self.model.add(Dropout(0.5))
-        self.model.add(LSTM(12, return_sequences=True))
-        self.model.add(LSTM(12, return_sequences=True))
-        self.model.add(Dropout(0.5))
-        self.model.add(LSTM(7))
+        self.model.add(
+            LSTM(100, input_shape=(self.back_step, self.features), activation="softsign", return_sequences=True))
+        self.model.add(LSTM(80, activation="softsign", return_sequences=True))
+        self.model.add(LSTM(60, activation="softsign", return_sequences=True))
+        self.model.add(Dropout(0.2))
+        self.model.add(LSTM(60, activation="softsign", return_sequences=True))
+        self.model.add(LSTM(60, activation="softsign", return_sequences=True))
+        self.model.add(Dropout(0.2))
+        self.model.add(LSTM(30, activation="softsign"))
         self.model.add(Dense(self.future_step))
         self.model.summary()
         self.model.compile(loss='mse', optimizer='adam', metrics=['accuracy'])
 
-    def train(self, data_x, data_y, epochs=800, batch=32, log_level=2):
-        self.model.fit(data_x, data_y, validation_split=0.1, epochs=epochs, batch_size=batch, verbose=log_level)
+    # training model
+    def train(self, data_x, data_y, epochs=600, batch=64, log_level=2):
+        results = self.model.fit(data_x, data_y, validation_split=0.1, epochs=epochs, batch_size=batch,
+                                 verbose=log_level)
 
-    def verify(self):
-        pass
+        # plotting loss
+        history = results.history
+        plt.figure(figsize=(12, 4))
+        plt.plot(history['val_loss'])
+        plt.plot(history['loss'])
+        plt.legend(['val_loss', 'loss'])
+        plt.title('Loss')
+        plt.xlabel('Epochs')
+        plt.ylabel('Loss')
+        plt.show()
+
+        # plotting accuracy
+        plt.figure(figsize=(12, 4))
+        plt.plot(history['val_accuracy'])
+        plt.plot(history['accuracy'])
+        plt.legend(['val_accuracy', 'accuracy'])
+        plt.title('Accuracy')
+        plt.xlabel('Epochs')
+        plt.ylabel('Accuracy')
+        plt.show()
+
+    def verify(self, data_x: np.array, data_y: np.array, scaler: MinMaxScaler):
+        plt.figure(figsize=(12, 5))
+        # getting time to add as timestamp to graph
+        date_time = datetime.now()
+        date_time_formatted = date_time.strftime("%d/%m/%Y-%H:%M")
+
+        # Getting predictions by predicting from the last X
+        prediction = self.model.predict(data_x[-1].reshape(1, self.back_step, self.features)).tolist()[0]
+
+        # Transforming normalized values back to normal range
+        prediction = scaler.inverse_transform(np.array(prediction).reshape(-1, 1))
+
+        # Getting the actual values from the last available y variable which correspond to its respective X variable
+        actual = scaler.inverse_transform(data_y[-1].reshape(-1, 1))
+
+        # Printing and plotting those predictions
+        print("Predicted Prices:\n", prediction)
+        plt.plot(prediction, label='Predicted')
+
+        # Printing and plotting the actual values
+        print("\nActual Prices:\n", actual.tolist())
+        plt.plot(actual.tolist(), label='Actual')
+
+        plt.title(f"Predicted vs Actual Closing Prices")
+        plt.ylabel("Price")
+        plt.legend()
+        plt.show()
+        # plt.savefig(f"../graphs/result-{date_time_formatted}.png")
--- src/utils/util.py
+++ src/utils/util.py
@@ -1,10 +1,15 @@
 import numpy as np
 import pandas as pd
 from pandas import DataFrame
+from sklearn.preprocessing import MinMaxScaler
 
 
 class Util:
     data_path = "../dataset/prices.csv"
+
+    def __init__(self):
+        self.scaler_x = MinMaxScaler()
+        self.scaler_y = MinMaxScaler()
 
     """
     Loading data from csv.
@@ -12,12 +17,15 @@ class Util:
     recent - number of most recent data
     """
 
-    def load(self, recent=1000):
+    def load(self, recent: int):
         data = pd.read_csv(self.data_path).tail(recent)
         data = data.set_index('timestamp')
         data.index = pd.to_datetime(data.index, unit='s')
 
-        return data
+        data_x = data[['open', 'high', 'low', 'volumeTo', 'volumeFrom']]
+        data_y = data['close']
+
+        return data_x, data_y
 
     """
     Preparing data for LSTM network.
@@ -27,57 +35,62 @@ class Util:
     future_step - number of days to predict
     """
 
-    def prepare(self, back_step: int, future_step: int):
+    def prepare(self, back_step: int, future_step: int, last_samples=None):
+        data_x, data_y = self.load(last_samples)
+
         # getting rid of invalid values
-        data = self.load().replace(0, np.nan)
-        data = data.fillna(data.min())
+        data_x = data_x.replace(0, np.nan).fillna(data_x.min())
+        data_y = data_y.replace(0, np.nan).fillna(data_y.min())
 
-        # min max normalization
-        normalized_data = (data - data.min()) / (data.max() - data.min())
+        train_x, train_y, test_x, test_y = self.split(data_x, data_y, 0.2)
 
-        train_data, test_data = self.split(normalized_data, 0.2)
-
-        train_x, train_y = self.create_window(train_data, back_step, future_step)
-        test_x, test_y = self.create_window(test_data, back_step, future_step)
+        train_x, train_y = self.create_window(train_x, train_y, back_step, future_step)
+        test_x, test_y = self.create_window(test_x, test_y, back_step, future_step)
 
         return train_x, train_y, test_x, test_y
 
     """
     Window method transformation.
     
-    data - data which will be separated to features and targets
+    data_x - features which will be separated to time windows
+    data_y - targets which will be separated to time windows
     back_step - number of days looking back
     future_step - number of days to predict
     """
 
-    def create_window(self, data: DataFrame, back_step: int, future_step: int):
+    def create_window(self, data_x: DataFrame, data_y: DataFrame, back_step: int, future_step: int):
         window_x, window_y = [], []
 
-        x_data = data[['open', 'high', 'low', 'volumeTo', 'volumeFrom']]
-        y_data = data['close']
+        # min max scaling
+        normalized_x = pd.DataFrame(self.scaler_x.fit_transform(data_x), columns=data_x.columns, index=data_x.index)
+        normalized_y = pd.DataFrame(self.scaler_y.fit_transform(data_y.values.reshape(-1, 1))).values.flatten()
 
-        for i in range(len(data)):
+        for i in range(len(data_x)):
             prev_days = i + back_step
             fut_days = prev_days + future_step
 
-            if fut_days > len(data):
+            if fut_days > len(data_x):
                 break
-
-            window_x.append(x_data.values[i:prev_days])
-            window_y.append(y_data.values[prev_days:fut_days])
+            window_x.append(normalized_x.values[i:prev_days])
+            window_y.append(normalized_y[prev_days:fut_days])
 
         return np.array(window_x), np.array(window_y)
 
     """
     Splitting data to train and test sets.
     
-    data - data to split
+    data_x - features to split
+    data_y - targets to split
     test_size - percentage of test set (from 0.0 to 1.0)
     """
 
-    def split(self, data: DataFrame, test_size: float):
-        split_row = len(data) - int(test_size * len(data))
-        train_data = data.iloc[:split_row]
-        test_data = data.iloc[split_row:]
+    def split(self, data_x: DataFrame, data_y: DataFrame, test_size: float):
+        split_row = len(data_x) - int(test_size * len(data_x))
 
-        return train_data, test_data
+        train_x = data_x.iloc[:split_row]
+        test_x = data_x.iloc[split_row:]
+
+        train_y = data_y.iloc[:split_row]
+        test_y = data_y.iloc[split_row:]
+
+        return train_x, train_y, test_x, test_y
