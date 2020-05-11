c4e7335f0b6f32f40176d77df9930463bbd7f8bf
Erik Dvorčák <erik.dvorcak@gmail.com>
2020-05-11 16:01:09 +0200

    final of tunning

--- src/stages/execution/lstm/lstm_bitcoin.py
+++ src/stages/execution/lstm/lstm_bitcoin.py
@@ -12,7 +12,7 @@ class LSTMBitcoin(Builder):
 
     def build(self):
         random.seed(1337)
-        self.model.add(LSTM(38, input_shape=(self.get_attributes('past_steps'), self.get_attributes('features'))))
+        self.model.add(LSTM(37, input_shape=(self.get_attributes('past_steps'), self.get_attributes('features'))))
         self.model.add(Dense(self.get_attributes('future_steps')))
 
         self.model.compile(loss='mae', optimizer='adam',
